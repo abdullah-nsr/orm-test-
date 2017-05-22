3ca3ffb63d2d2c81dffc23bc3c2c685a4d741aa2
abdullah <abdullah.nasri@outlook.sa>
2017-05-22 16:21:59 +0200

    create student class and database

--- entity/Course.ts
+++ entity/Course.ts
@@ -1,5 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
-import { Student } from "./student";
+import { Student } from "./Student";
 
 @Entity()
 export class Course {
--- index.ts
+++ index.ts
@@ -2,24 +2,40 @@ import express = require("express");
 import "reflect-metadata";
 import { createConnection, Connection } from "typeorm";
 import { Course } from "./entity/Course";
+import { Student } from "./entity/Student"
 
 let app = express();
 let conn: Connection;
 
 createConnection().then(connection => {
     conn = connection
+  
     let c = new Course();
     c.name = "Course 1"
     c.start_date = new Date();
-    
-    let repo = connection.getRepository(Course);
-    repo.persist(c);
+
+    let courserepo = connection.getRepository(Course);
+    courserepo.persist(c);
+
+    let s = new Student();
+    s.firstname = "abdul";
+    s.lastname = "nsr";
+    s.start_date = new Date();
+  
+    let studentrepo = connection.getRepository(Student);
+    studentrepo.persist(s);
+    console.log("is done");
 
         app.get("/list", (req, res) => {
          conn.getRepository(Course).find().then(courses => { 
            res.send(JSON.stringify(courses));
           });   
      });
+     app.get("/student", (req, res) => {
+         conn.getRepository(Student).find().then(student => { 
+           res.send(JSON.stringify(student));
+          });   
+     });    
 }).catch(error => console.log(error));
 
 app.listen(3000);
